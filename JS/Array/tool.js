c7c81f1ca0181ab8df8e01e0e69574ca0cb38877
Iamanuan <gemma.mobu@icloud.com>
2019-11-13 10:05:46 +0800

    65-66

---
+++ JS/Array/tool.js
@@ -0,0 +1,28 @@
+//冒泡排序
+function bubble(arr){
+	for (var i = 0; i < arr.length; i++) { //比较的轮次
+		//每一轮比较的次数
+		for (var j = 0; j < arr.length - i - 1; j++) {
+			//判断前后两个个数是否符合交换条件
+			if(arr[j] > arr[j + 1]){
+				var tmp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+
+}
+
+//选择排序
+function choose(arr){
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = i + 1; j < arr.length; j++) {
+			if(arr[i] > arr[j]){
+				var tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+}
