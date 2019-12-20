91c0187578ed6533ffbb405c540d0f8e7723d2ab
Iamanuan <gemma.mobu@icloud.com>
2019-12-20 12:00:27 +0800

    151

---
+++ JS/ESM/00_AMD/math.js
@@ -0,0 +1,18 @@
+define(function () {
+    var PI = 3.1415926;
+
+    function multiple(num1,num2){
+        return num1 * num2;
+    }
+
+    function square(n) {
+        // return n * n;
+        return Math.pow(n,2);
+    }
+
+    return{
+        PI: PI,
+        multiple: multiple,
+        square:square
+    }
+})
